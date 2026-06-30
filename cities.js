93693f80863eb0e51096cb4f0e8f32535e3515af
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:30:33 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
 const michiganCities = [
-  // paste first 80 cities from cities.csv here
+  "Washington", "Ada", "Detroit", "Adrian", "Traverse City",
+  "Davisburg", "Eaton Rapids", "Burr Oak", "Northville", "Beulah",
+  "Climax", "Bark River", "Barton City", "Chesaning", "Boyne City",
+  "Bay City", "Barryton", "Bad Axe", "Allen Park", "Bangor",
+  "Bronson", "Battle Creek", "Caro", "Belleville", "Carson City",
+  "Au Gres", "Carleton", "Alma", "Bridgeport", "Big Rapids",
+  "Armada", "Bloomfield Hills", "Bancroft", "Albion", "Blissfield",
+  "Allegan", "Burt", "Brown City", "Breckenridge", "Brutus",
+  "Clare", "Addison", "Brooklyn", "Charlevoix", "Algonac",
+  "Center Line", "Cass City", "Berrien Center", "Williamston", "Felch",
+  "Franklin", "Kalamazoo", "Mount Clemens", "Livonia", "White Lake",
+  "Tecumseh", "Holt", "Grand Rapids", "Clinton Township", "Sterling Heights",
+  "Britton", "Chase", "Bellevue", "Commerce Township", "Canton",
+  "Allenton", "Cedar Springs", "Benton Harbor", "Augusta", "Akron",
+  "East China", "Monroe", "Warren", "Saint Clair Shores", "West Bloomfield",
+  "South Lyon", "Belmont", "Ann Arbor", "Chassell", "Auburn",
 ];
 
 const BATCH_SIZE = 5;
